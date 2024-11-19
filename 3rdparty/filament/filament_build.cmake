c494cb40b5da76ad696af809affe65f5a525b83c
Giosuè Zannini <zannini1994@gmail.com>
2024-11-19 17:00:37 +0100

    Update filament_build.cmake with fixed version of filament

--- 3rdparty/filament/filament_build.cmake
+++ 3rdparty/filament/filament_build.cmake
@@ -52,8 +52,8 @@ endif()
 ExternalProject_Add(
     ext_filament
     PREFIX filament
-    URL https://github.com/isl-org/filament/archive/a9c5bbf18597b80b0717e56f59c42ad39de7cbdb.tar.gz
-    URL_HASH SHA256=ea5b9a6a2985922e4028cd212ee346c1232586576368c2ea0ef3630d3054eb25
+    URL  https://github.com/isl-org/filament/archive/98f0c617ac61424c1ebfee26f9d51c3d13c80d6c.tar.gz
+    URL_HASH SHA256=714522eeaf9c59ba7774e21a85b4a62a6cc3f867303503783ce453267f2da225
     DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/filament"
     UPDATE_COMMAND ""
     CMAKE_ARGS
